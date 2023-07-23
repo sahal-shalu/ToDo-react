b1871c5ad18fdd7a9cd765017c6985a38d26f6d0
sahal-shalu <sahalshalu5426@gmail.com>
2023-07-23 19:56:28 +0530

    todo completed with react

---
+++ src/AddTask.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+function AddTask({addTask}) {
+    const[value,setValue]=useState('')
+    const addItem=()=>{addTask(value)
+    setValue("")}
+    
+  return (
+    <>
+    <div className='input-contianer'>
+        <input type='text' className='input' placeholder='Add a new task'
+        value={value}
+        onChange={(e)=>{setValue(e.target.value)}}/>
+        <button onClick={addItem} className='add-btn'>ADD</button>
+</div> 
+    </>
+  )
+}
+
+export default AddTask
